79fb30b6ed97a8d1533bcb5ceaf14d6baa1003b1
dimasm93 <software.dimas_m@icloud.com>
2019-10-22 15:31:20 +0700

    template quis

---
+++ src/main/java/com/tabeldata/bootcamp/Bootcamp2019Application.java
@@ -0,0 +1,13 @@
+package com.tabeldata.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp2019Application.class, args);
+	}
+
+}
